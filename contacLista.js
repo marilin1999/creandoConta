e6ea4a4b326bf9c0c2ee9de186f49fbeac87c3dc
¨marilin¨ <sebastianjmarimon@gmail.com>
2023-09-02 01:30:34 -0500

    todos los cambios

--- contacLista.js
+++ contacLista.js
@@ -7,21 +7,21 @@ let crearContacto = [
     },
     { id: 2,
         Nombre: "sebastian", 
-        Apellido: "jimenez",
+        Apellido: "ramos",
         Telefono: "300679", 
-        Ubicacion: {ciudad: "cartagena", direccion: 35},
+        Ubicacion: {ciudad: "medellin", direccion: 35},
        },
        { id: 3,
-        Nombre: "magalis", 
+        Nombre: "sofia", 
         Apellido: "mestra",
         Telefono: "3133625", 
-        Ubicacion: {ciudad: "cartagena", direccion: 91},
+        Ubicacion: {ciudad: "bogota", direccion: 91},
        },
        { id: 4,
         Nombre: "andrea", 
         Apellido: "rodriguez",
         Telefono: "376288", 
-        Ubicacion: {ciudad: "cartagena", direccion: 56},
+        Ubicacion: {ciudad: "barranquilla", direccion: 56},
        },
 ];
 
@@ -42,20 +42,20 @@ function agregarContacto(nuevoContacto){
     Nombre: "sandra", 
     Apellido: "perez",
     Telefono: "376856", 
-    Ubicacion: {ciudad: "medellin", direccion: 89},
+    Ubicacion: {ciudad: "guajira", direccion: 89},
    };
     
    function actualiContact(idBuscando, nuevosDatos){
-let actualicon = crearContacto.findIndex (contacto => contacto.id === idBuscando);
-  if (actualicon !== -1) {
-
-    Object.assign(crearContacto[actualicon],  nuevosDatos);
-    console.log("contacto editado.");
-  }
-  else{
-    console.log("no realizado");
-  }
-}
+    let actualicon = crearContacto.findIndex (contacto => contacto.id === idBuscando);
+      if (actualicon !== -1) {
+    
+        Object.assign(crearContacto[actualicon],  nuevosDatos);
+        console.log("contacto editado.");
+      }
+      else{
+        console.log("no realizado");
+      }
+    }
 
   
    
@@ -63,6 +63,6 @@ let actualicon = crearContacto.findIndex (contacto => contacto.id === idBuscando
    imprimirContactos(),
    borrarContacto(5),
   imprimirContactos()
-   actualiContact(2, {Apellido: "nudo"})
+   actualiContact(2, {Apellido: "perez"})
    
-         
+          
